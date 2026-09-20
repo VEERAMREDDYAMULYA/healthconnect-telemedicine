dea487384058b8c912ba1dd7f48536f248c22d11
Abhinav7Turlapati <2400032990@kluniversity.in>
2026-09-20 10:02:12 +0530

    Add Eureka Server

---
+++ eureka-server/src/test/java/com/healthconnect/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthconnect.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
